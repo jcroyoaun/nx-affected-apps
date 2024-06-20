de75a5e5ea6d1c2a493326434149ef16d9f1783b
JuanCarlos Martinez <jmartinez01@petsmart.com>
2024-06-19 22:21:01 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const { Workspaces } = require('@nrwl/devkit');
+const { execSync } = require('child_process');
+const { join } = require('path');
+
+try {
+  const tag = core.getInput('tag', { required: true });
+
+  const workspace = new Workspaces(
+    join(process.cwd(), '..')
+  ).readWorkspaceConfiguration();
+
+  const projects = execSync('npx nx show projects --affected')
+    .toString('utf-8')
+    .trim()
+    .split('\n')
+    .filter((project) => !!project);
+
+  const affected = projects.filter((project) =>
+    workspace.projects[project].tags?.includes(tag)
+  );
+
+  const affectedString = affected.join(' ');
+
+  core.setOutput('affected_projects', affectedString);
+} catch (error) {
+  core.setFailed(error.message);
+}
