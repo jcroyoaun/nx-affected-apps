5aac0fb8b3c6dc9b87290729e4a937b67d9656a8
JuanCarlos Martinez <jmartinez01@petsmart.com>
2024-06-20 01:18:07 -0600

    rolling back some versions

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ const { Workspaces } = require('@nrwl/devkit');
 const { execSync } = require('child_process');
 const { join } = require('path');
 
-try {
-  const tag = core.getInput('tag', { required: true });
+const [tag = null] = process.argv.slice(2);
 
-  const workspace = new Workspaces(
-    join(process.cwd(), '..')
-  ).readWorkspaceConfiguration();
-
-  const projects = execSync('yarn -s nx show projects --affected') 
-    .toString('utf-8')
-    .trim()
-    .split('\n')
-    .filter((project) => !!project);
-
-  const affected = projects.filter((project) =>
-    workspace.projects[project].tags?.includes(tag)
-  );
-
-  const affectedString = affected.join(' ');
-
-  core.setOutput('affected_projects', affectedString);
-} catch (error) {
-  core.setFailed(error.message);
+if (!tag) {
+  console.error(`Missing tag`);
+  process.exit(1);
 }
+
+const workspace = new Workspaces(
+  join(__dirname, '..')
+).readWorkspaceConfiguration();
+
+const projects = execSync('npx nx show projects --affected') 
+  .toString('utf-8')
+  .trim()
+  .split('\n')
+  .filter((project) => !!project);
+
+const affected = projects.filter((project) =>
+  workspace.projects[project].tags?.includes(tag)
+);
+
+const affectedString = `${affected.join(' ')}`;
+
+console.log(affectedString);
