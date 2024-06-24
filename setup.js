ae49165b84df200b5283be84047bf2da44e2ba3d
JuanCarlos Martinez <jmartinez01@petsmart.com>
2024-06-24 01:12:08 -0600

    test

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 try {
   console.log('Installing dependencies...');
-  execSync('npm install @nrwl/devkit@16.9.1 nx@16.9.1', { stdio: 'inherit' });
+  execSync('npm install @actions/core@1.10.0 @nrwl/devkit@16.9.1 nx@16.9.1', { stdio: 'inherit' });
 } catch (error) {
   console.error('Failed to install dependencies:', error);
   process.exit(1);
