7746cba3c920e2823c20df8ccb75c98e584720aa
JuanCarlos Martinez <jmartinez01@petsmart.com>
2024-06-24 01:41:07 -0600

    test-removing this thing

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 // Setup: Install dependencies locally
-try {
-  console.log('Installing dependencies...');
-  execSync('npm init -y && npm install @actions/core@1.10.0 @nrwl/devkit@16.9.1 nx@16.9.1 --save', { stdio: 'inherit' });
-} catch (error) {
-  console.error('Failed to install dependencies:', error);
-  process.exit(1);
-}
+//try {
+  //console.log('Installing dependencies...');
+  //execSync('npm init -y && npm install @actions/core@1.10.0 @nrwl/devkit@16.9.1 nx@16.9.1 --save', { stdio: 'inherit' });
+//} catch (error) {
+//  console.error('Failed to install dependencies:', error);
+//  process.exit(1);
+//}
 
 // Add the local node_modules to the require path
 process.env.NODE_PATH = path.join(process.cwd(), 'node_modules');
