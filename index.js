14567e2fc2619bc12b042468d00deb962f4c593e
JuanCarlos Martinez <jmartinez01@petsmart.com>
2024-06-24 16:03:46 -0600

    adding new tags

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
-// Setup: Install dependencies locally
-//try {
-  //console.log('Installing dependencies...');
-  //execSync('npm init -y && npm install @actions/core@1.10.0 @nrwl/devkit@16.9.1 nx@16.9.1 --save', { stdio: 'inherit' });
-//} catch (error) {
-//  console.error('Failed to install dependencies:', error);
-//  process.exit(1);
-//}
-
-// Add the local node_modules to the require path
-process.env.NODE_PATH = path.join(process.cwd(), 'node_modules');
-require('module').Module._initPaths();
-
-// Main logic
 const core = require('@actions/core');
 const { Workspaces } = require('@nrwl/devkit');
 
 try {
-  const tag = core.getInput('tag', { required: true });
+  const frontendTag = core.getInput('frontend_tag');
+  const backendTag = core.getInput('backend_tag');
+  const includeLibs = core.getBooleanInput('include_libs');
+  const allProjects = core.getBooleanInput('all_projects');
 
   const workspace = new Workspaces(process.cwd()).readWorkspaceConfiguration();
-
   const projects = execSync('npx nx show projects --affected')
     .toString('utf-8')
     .trim()
     .split('\n')
     .filter((project) => !!project);
 
-  const affected = projects.filter((project) =>
-    workspace.projects[project].tags?.includes(tag)
+  const frontendProjects = projects.filter((project) =>
+    workspace.projects[project].tags?.includes(frontendTag)
+  );
+  const backendProjects = projects.filter((project) =>
+    workspace.projects[project].tags?.includes(backendTag)
   );
 
-  const affectedString = affected.join(' ');
+  let affectedProjects = [];
+  if (allProjects) {
+    affectedProjects = projects;
+  } else {
+    affectedProjects = [...frontendProjects, ...backendProjects];
+    if (includeLibs) {
+      const libraryProjects = projects.filter((project) =>
+        workspace.projects[project].tags?.includes('type:lib')
+      );
+      affectedProjects = [...affectedProjects, ...libraryProjects];
+    }
+  }
 
-  console.log(affectedString);
-  core.setOutput('affected_projects', affectedString);
+  const frontendString = frontendProjects.join(' ');
+  const backendString = backendProjects.join(' ');
+  const projectsString = affectedProjects.join(' ');
+
+  core.setOutput('frontend_components', frontendString);
+  core.setOutput('backend_components', backendString);
+  core.setOutput('projects', projectsString);
 } catch (error) {
   console.error('Error:', error);
   process.exit(1);
